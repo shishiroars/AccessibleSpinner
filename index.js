76da6685bd333a04e56a2e8a3733e69fbf96d3a2
Shishir Shrivastava <shishir.sep@gmail.com>
2021-09-02 18:22:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,6 @@ import React from 'react';
 import { Spinner } from 'reactstrap';
 import './index.css';
 
-/**
- * Adds a React component to show a loading icon.
- *
- *  - Roles
- *     - Show a loading icon.
- *     - Cover up the complete screen so no action can be performed while the loading animation is active.
- *
- * @param isLoading if true, a loading animation is seen on the screen.
- */
-
 export const AccessibleSpinner = props => {
     return (
         <div className="spinner-container">
